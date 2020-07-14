b5c92e15a6c04751d6b480a4bd87d6576769fefe
Vedran Furtula <vfurtula@localhost.localdomain>
2020-07-14 16:57:57 +0200

    v200714

---
+++ instruments/A34972A.py
@@ -0,0 +1,112 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan 12 19:40:35 2018
+
+@author: Vedran Furtula
+"""
+
+
+import sys, serial, argparse, time, re, random, visa
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+
+class A34972A:
+	
+	def __init__(self,my_serial,testmode):
+		# activate the serial. CHECK the serial port name!
+		
+		self.testmode = testmode
+		if self.testmode:
+			print("Testmode: Agilent34972A port opened")
+		elif not self.testmode:
+			rm = visa.ResourceManager()
+			self.ser = rm.open_resource(my_serial)
+			time.sleep(0.25)
+		#self.ser=serial.Serial(my_serial,baudrate=19200,parity=serial.PARITY_NONE,stopbits=serial.STOPBITS_ONE)
+		#print("Agilent 34972A serial port:", my_serial))
+		#time.sleep(1)
+
+  ############################################################
+	# Check input if a number, ie. digits or fractions such as 3.141
+	# Source: http://www.pythoncentral.io/how-to-check-if-a-string-is-a-number-in-python-including-unicode/
+	def is_number(self,s):
+		try:
+			float(s)
+			return True
+		except ValueError:
+			pass
+
+		try:
+			import unicodedata
+			unicodedata.numeric(s)
+			return True
+		except (TypeError, ValueError):
+			pass
+		
+		return False
+	
+	####################################################################
+	# K2001A functions
+	####################################################################
+	
+	def return_id(self):
+		if self.testmode:
+			return "Testmode: return_id A34972A"
+		elif not self.testmode:
+			val = self.ser.query("*idn?")
+			return val
+	
+	
+	def set_dc_voltage(self):
+		if self.testmode:
+			return "Testmode: set_dc_voltage A34972A"
+		elif not self.testmode:
+			#read digitized voltage value from the analog port number (dev)
+			self.ser.write(":conf:volt[:dc]")
+	
+	
+	def return_voltage(self,*argv):
+		if self.testmode:
+			time.sleep(0.1)
+			if argv:
+				return argv[0]+random.uniform(-1,1)
+			else:
+				return random.uniform(-1,1)
+		elif not self.testmode:
+			#read digitized voltage value from the analog port number (dev)
+			val = self.ser.query("meas:volt:dc? 1,0.003,(@101)")
+			if self.is_number(val):
+				#print("return_reffreq: ", val)
+				return float(val)
+			else:
+				print("Bad value returned from Agilent 34972A (read command):", val)
+				return ValueError
+		
+		
+	def close(self):
+		if self.testmode:
+			print("Testmode: Agilent 34972A port flushed and closed")
+		elif not self.testmode:
+			#self.ser.close()
+			print("Status: Agilent 34972A port flushed and closed")
+		
+		
+def main():
+  
+	# call the sr510 port
+	model_510 = A34972A('USB0::0x0957::0x2007::MY49017249::0::INSTR',False)
+	
+	print(model_510.return_id())
+	
+	for i in range(10):
+		print(model_510.return_voltage())
+		time.sleep(1)
+	
+if __name__ == "__main__":
+	
+  main()
+  
+
+
---
+++ test_dialogs/Oriel_TEST_dialog.py
@@ -0,0 +1,254 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan 12 09:06:01 2018
+
+@author: Vedran Furtula
+"""
+
+import os, sys, re, serial, time, numpy, configparser
+
+from PyQt5.QtCore import QThreadPool
+from PyQt5.QtWidgets import (QDialog, QMessageBox, QGridLayout,
+														 QLabel, QLineEdit, QComboBox, QVBoxLayout, QHBoxLayout, QPushButton)
+
+from instruments import Oriel_stepper
+
+
+
+
+class Oriel_TEST_dialog(QDialog):
+	
+	def __init__(self, parent, inst_list):
+		super().__init__(parent)
+		# constants
+		self.inst_list = inst_list
+		
+		# Initial read of the config file
+		self.config = configparser.ConfigParser()
+		
+		try:
+			self.config.read('config.ini')
+			
+			self.orielport_str = self.config.get("Instruments",'orielport').strip().split(',')[0]
+		except configparser.NoOptionError as e:
+			QMessageBox.critical(self, 'Message',''.join(["Main FAULT while reading the config.ini file\n",str(e)]))
+			raise
+		
+		self.target = " "
+		
+		self.setupUi()
+		
+		# close both MS257 serial ports if open
+		if self.inst_list.get("Oriel"):
+			self.inst_list.get("Oriel").close()
+		self.inst_list.pop('Oriel', None)
+		
+		
+	def setupUi(self):
+		serialport = QLabel("Serial port",self)
+		self.serialportEdit = QLineEdit(self.orielport_str,self)
+		
+		self.upButton = QPushButton(u'\u25b2',self)
+		self.set_bstyle_v1(self.upButton)
+		self.downButton = QPushButton(u'\u25bc',self)
+		self.set_bstyle_v1(self.downButton)
+		
+		pos_lbl = QLabel('Stored target position:',self)
+		self.tarpos_lbl = QLabel('',self)
+		self.tarpos_lbl.setStyleSheet("color: blue")
+		
+		self.setButton = QPushButton('Set position target',self)
+		self.settarEdit = QLineEdit('',self)
+		self.gotoButton = QPushButton('Move to target',self)
+		
+		target_lbl = QLabel("Get target ",self)
+		self.combo0 = QComboBox(self)
+		self.combo0.setFixedWidth(85)
+		self.mylist0=["A","B", " "]
+		self.combo0.addItems(self.mylist0)
+		self.combo0.setCurrentIndex(self.mylist0.index(self.target))
+		
+		##############################################
+		
+		g0_1 = QGridLayout()
+		g0_1.addWidget(serialport,0,0)
+		g0_1.addWidget(self.serialportEdit,0,1)
+		g0_1.addWidget(target_lbl,0,2)
+		g0_1.addWidget(self.combo0,0,3)
+		
+		g0_2 = QGridLayout()
+		g0_2.addWidget(self.upButton,0,0)
+		g0_2.addWidget(self.downButton,1,0)
+		
+		g1_1 = QGridLayout()
+		g1_1.addWidget(pos_lbl,0,0)
+		g1_1.addWidget(self.tarpos_lbl,0,1)
+		
+		g2_1 = QGridLayout()
+		g2_1.addWidget(self.setButton,0,0)
+		g2_1.addWidget(self.settarEdit,0,1)
+		g2_1.addWidget(self.gotoButton,0,2)
+		
+		vbox1 = QVBoxLayout()
+		vbox1.addLayout(g1_1)
+		vbox1.addLayout(g2_1)
+		
+		hbox2 = QHBoxLayout()
+		hbox2.addLayout(g0_2)
+		hbox2.addLayout(vbox1)
+		
+		vbox2 = QVBoxLayout()
+		vbox2.addLayout(g0_1)
+		vbox2.addLayout(hbox2)
+		
+		##############################################
+		
+		self.threadpool = QThreadPool()
+		print("Multithreading in the Oriel dialog with maximum %d threads" % self.threadpool.maxThreadCount())
+		
+		self.setLayout(vbox2)
+		self.setWindowTitle("Test Oriel stepper controller")
+		
+		# Initialize and set titles and axis names for both plots
+		self.combo0.activated[str].connect(self.onActivated0)
+		
+		# run or cancel the main script
+		self.gotoButton.clicked.connect(self.runstop)
+		self.setButton.clicked.connect(self.runstop)
+		self.upButton.clicked.connect(self.runstop)
+		self.downButton.clicked.connect(self.runstop)
+		
+		
+	def set_bstyle_v1(self,button):
+		button.setStyleSheet('QPushButton {font-size: 25pt}')
+		button.setFixedWidth(40)
+		button.setFixedHeight(40)
+		
+		
+	def is_number(self,s):
+		try:
+			float(s)
+			return True
+		except ValueError:
+			pass
+
+		try:
+			import unicodedata
+			unicodedata.numeric(s)
+			return True
+		except (TypeError, ValueError):
+			pass
+
+		return False
+	
+	
+	def onActivated0(self, text):
+		
+		if not hasattr(self, 'Oriel'):
+			try:
+				self.Oriel = Oriel_stepper.Oriel_stepper(str(self.serialportEdit.text()), False)
+			except Exception as e:
+				reply = QMessageBox.critical(self, 'Oriel stepper TEST MODE', ''.join(["Oriel stepper could not return valid echo signal. Check the port name and check the connection.\n",str(e),"\n\nDo you want to proceed into the TEST MODE?"]), QMessageBox.Yes | QMessageBox.No)
+				if reply == QMessageBox.Yes:
+					self.Oriel = Oriel_stepper.Oriel_stepper(str(self.serialportEdit.text()), True)
+				else:
+					self.combo0.setCurrentIndex(self.mylist0.index(self.target))
+					return
+				
+		self.orielport_str = str(self.serialportEdit.text())
+		
+		self.serialportEdit.setEnabled(False)
+		self.settarEdit.setEnabled(False)
+		self.combo0.setEnabled(False)
+		self.gotoButton.setEnabled(False)
+		self.setButton.setEnabled(False)
+		self.upButton.setEnabled(False)
+		self.downButton.setEnabled(False)
+		
+		self.target = str(text)
+		
+		if self.target=="A":
+			tal = self.Oriel.return_ta()
+			self.tarpos_lbl.setText(str(tal))
+		elif self.target=="B":
+			tal = self.Oriel.return_tb()
+			self.tarpos_lbl.setText(str(tal))
+		else:
+			QMessageBox.warning(self, 'Message',"Set the position target to A or to B")
+			
+		self.set_finished()
+		
+	def runstop(self):
+		
+		if not hasattr(self, 'Oriel'):
+			try:
+				self.Oriel = Oriel_stepper.Oriel_stepper(str(self.serialportEdit.text()), False)
+			except Exception as e:
+				reply = QMessageBox.critical(self, 'Oriel stepper TEST MODE', ''.join(["Oriel stepper could not return valid echo signal. Check the port name and check the connection.\n",str(e),"\n\nShould the program proceeds into the TEST MODE?"]), QMessageBox.Yes | QMessageBox.No)
+				if reply == QMessageBox.Yes:
+					self.Oriel = Oriel_stepper.Oriel_stepper(str(self.serialportEdit.text()), True)
+				else:
+					self.combo0.setCurrentIndex(self.mylist0.index(self.target))
+					return
+		
+		self.orielport_str = str(self.serialportEdit.text())
+		
+		self.serialportEdit.setEnabled(False)
+		self.settarEdit.setEnabled(False)
+		self.combo0.setEnabled(False)
+		self.gotoButton.setEnabled(False)
+		self.setButton.setEnabled(False)
+		self.upButton.setEnabled(False)
+		self.downButton.setEnabled(False)
+		
+		sender=self.sender()
+		
+		if sender.text()=="Set position target":
+			if not self.is_number(str(self.settarEdit.text())):
+				QMessageBox.warning(self, 'Message',"The target position should be a real number")
+			else:
+				if self.target=="A":
+					self.Oriel.set_ta(str(self.settarEdit.text()))
+				elif self.target=="B":
+					self.Oriel.set_tb(str(self.settarEdit.text()))
+				
+		elif sender.text()=="Move to target":
+			if self.target=="A":
+				self.Oriel.goto_a()
+				tal = self.Oriel.return_ta()
+				self.tarpos_lbl.setText(str(tal))
+			elif self.target=="B":
+				self.Oriel.goto_b()
+				tal = self.Oriel.return_tb()
+				self.tarpos_lbl.setText(str(tal))
+			else:
+				QMessageBox.warning(self, 'Message',"The target position is specified. Choose A or B in the set position targen field.")
+			
+		elif sender.text()==u'\u25b2':
+			self.Oriel.jog_up()
+			
+		elif sender.text()==u'\u25bc':
+			self.Oriel.jog_down()
+		
+		self.set_finished()
+			
+	def set_finished(self):
+		self.serialportEdit.setEnabled(True)
+		self.settarEdit.setEnabled(True)
+		self.combo0.setEnabled(True)
+		self.gotoButton.setEnabled(True)
+		self.setButton.setEnabled(True)
+		self.upButton.setEnabled(True)
+		self.downButton.setEnabled(True)
+		
+		
+	def closeEvent(self, event):
+		reply = QMessageBox.question(self, 'Message', "Quit now? Changes will not be saved!", QMessageBox.Yes | QMessageBox.No)
+		if reply == QMessageBox.Yes:
+			if hasattr(self, 'Oriel'):
+				self.Oriel.close()
+			
+			event.accept()
+		else:
+		  event.ignore() 
